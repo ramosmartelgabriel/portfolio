c4dd6aed7f728a18608763f8f56ade7e13eda707
Gabriel Martel <ramosmartelgabriel.com>
2026-01-05 23:42:30 -0300

    Criando primeiro arquivo .py

---
+++ Main.py
@@ -0,0 +1,5 @@
+# Main.py criada em 2025-02-05
+
+# Quero criar uma função que calcule a soma de dois números
+def soma(a, b):
+    return a + b
--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
 --Start Date: 2026-01-05 23:18:00
+
+--Primeiro projeto: catálogo de Pokémon
